2a7d6745b823dc13207e6e2a5a9ce7d610a5c053
Ruben Oostinga <ruben.oostinga@gmail.com>
2024-06-14 21:47:23 +0200

    test almost working

--- tests/rankforce.spec.ts
+++ tests/rankforce.spec.ts
@@ -1,4 +1,9 @@
-import { expect, test } from '@playwright/test';
+import { expect, Page, test } from '@playwright/test';
+
+async function fillUsername(page: Page, username: string) {
+  await page.getByLabel('Username').fill(username);
+  await page.getByText('Save').click();
+}
 
 test.describe('Item ranking', () => {
   test('should fill in username and remember it across tabs', async ({
@@ -7,8 +12,7 @@ test.describe('Item ranking', () => {
   }) => {
     await page.goto('/');
 
-    await page.getByLabel('Username').fill('testuser');
-    await page.getByText('Save').click();
+    await fillUsername(page, 'testuser');
 
     const newPage = await context.newPage();
     await newPage.goto(page.url());
@@ -16,29 +20,35 @@ test.describe('Item ranking', () => {
     expect(await newPage.getByLabel('Username')).toHaveValue('testuser');
   });
 
-  test('should fill in and store items', async ({ page }) => {
-    await page.goto('/');
+  test('should sync data between browsers', async ({ browser }) => {
+    const context1 = await browser.newContext();
+    const context2 = await browser.newContext();
 
-    await page.getByRole('textbox', { name: 'Item to rank:' }).fill('item1');
+    const page1 = await context1.newPage();
+    const page2 = await context2.newPage();
+
+    await page1.goto('/');
+    await page2.goto(page1.url());
+
+    await fillUsername(page1, 'user 1');
+    await fillUsername(page2, 'user 2');
+
+    await page1.getByText('Configure').click();
+    await page2.getByText('Configure').click();
+
+    await page1.getByRole('textbox', { name: 'Item to rank:' }).fill('item1');
 
     await Promise.all([
-      page.getByText('Add item').click(),
-      page.waitForRequest((request) => request.method() === 'POST'),
-      page.getByText('item1').waitFor({
+      page1.getByText('Add item').click(),
+      page1.waitForRequest((request) => request.method() === 'POST'),
+      page1.getByText('item1').waitFor({
         state: 'visible',
       }),
     ]);
 
-    // wait 2 secs
-    // await page.waitForTimeout(2000);
-
-    // wait for network request to complete
-
-    console.log('page.url()', page.url());
-    await page.goto(page.url());
-
-    await page.getByText('item1').waitFor({
+    await page2.getByText('item1').waitFor({
       state: 'visible',
+      timeout: 5000,
     });
   });
 });
