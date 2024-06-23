561b93eb02679eefe04529edc62c425e46bcc376
Ruben Oostinga <ruben.oostinga@gmail.com>
2024-06-23 14:10:58 +0200

    Parse token

--- src/function/realtime-authorizer.ts
+++ src/function/realtime-authorizer.ts
@@ -1,40 +1,5 @@
-import { MqttToken } from '@/shared/MqttToken';
+import { assertIsToken, validateToken } from '@/shared/MqttToken';
 import { realtime } from 'sst/aws/realtime';
-import { decodeTime } from 'ulid';
-
-export function assertIsString(value: unknown): asserts value is string {
-  if (typeof value !== 'string') {
-    throw new Error('Not a string');
-  }
-}
-
-export function assertIsNonEmptyString(
-  value: unknown,
-): asserts value is string {
-  assertIsString(value);
-  if (value === '') {
-    throw new Error('Empty string');
-  }
-}
-
-export function assertIsObject(
-  input: unknown,
-): asserts input is Record<string, unknown> {
-  if (typeof input !== 'object' || input === null || Array.isArray(input)) {
-    throw new Error('Not an object');
-  }
-}
-
-function assertIsToken(parsed: unknown): asserts parsed is MqttToken {
-  assertIsObject(parsed);
-  assertIsNonEmptyString(parsed.sessionId);
-  assertIsNonEmptyString(parsed.userId);
-}
-
-function validateToken(token: MqttToken) {
-  decodeTime(token.sessionId);
-  decodeTime(token.userId);
-}
 
 export const handler = realtime.authorizer(async (token) => {
   console.log('Authorizing token:', token);
---
+++ src/shared/type-assertions.ts
@@ -0,0 +1,22 @@
+export function assertIsString(value: unknown): asserts value is string {
+  if (typeof value !== 'string') {
+    throw new Error('Not a string');
+  }
+}
+
+export function assertIsNonEmptyString(
+  value: unknown,
+): asserts value is string {
+  assertIsString(value);
+  if (value === '') {
+    throw new Error('Empty string');
+  }
+}
+
+export function assertIsObject(
+  input: unknown,
+): asserts input is Record<string, unknown> {
+  if (typeof input !== 'object' || input === null || Array.isArray(input)) {
+    throw new Error('Not an object');
+  }
+}
