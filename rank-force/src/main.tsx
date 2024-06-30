2f03c8556a1d5b222541ec0a62e6dcf94f0214bd
Ruben Oostinga <ruben.oostinga@gmail.com>
2024-06-30 14:38:16 +0200

    install tailwind in vite app

--- rank-force/src/main.tsx
+++ rank-force/src/main.tsx
@@ -2,6 +2,7 @@ import 'reflect-metadata';
 import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider, createRouter } from '@tanstack/react-router';
+import './index.css';
 
 // Import the generated route tree
 import { routeTree } from './routeTree.gen';
